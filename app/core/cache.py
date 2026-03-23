1d2f6acc44a0819359bbd3d310441a2cb2f9dc51
Jingxu Lan <jingxulan98@gmail.com>
2026-03-23 14:44:15 -0400

    Fix: Make redis host dynamic

--- app/core/cache.py
+++ app/core/cache.py
@@ -1,10 +1,14 @@
 # app/core/cache.py
+import os
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis.asyncio import Redis
 from fastapi import FastAPI
 
 async def init_cache(app: FastAPI):
-    redis = Redis(host="redis", port=6379, db=0, decode_responses=True)
+    # redis = Redis(host="redis", port=6379, db=0, decode_responses=True)
+    # 动态获取 Redis Host，默认为 "redis"（照顾你本地的 docker-compose）
+    redis_host = os.getenv("REDIS_HOST", "redis")
+    redis = Redis(host=redis_host, port=6379, db=0, decode_responses=True)
     FastAPICache.init(RedisBackend(redis), prefix="policy-api")
     app.state.redis = redis
